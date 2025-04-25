d3999adcd0e8b2c7e851b229ad8116a2cddc5b4f
anitha6752 <168512301+anitha6752@users.noreply.github.com>
2025-04-25 15:58:11 +0530

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,37 @@
+print('hello', end=' ')
+print('world')
+print('hello', end=' not ')
+print('intrusted')
+age = 2
+print(type(age))
+price = 19.5
+print(type(price))
+is_sunny = True
+print(type(is_sunny))
+z = 3+4j
+print(type(z))
+
+fruits = ["apple","banana","cherry"]
+number = [1,2,3,4,5]
+mixed = [1,"apple",3.5,True]
+
+print(type(fruits))
+print(type(number))
+print(type (mixed))
+
+point = (3,4)
+person = ("john","green","blue")
+colour = ("red","green","purple")
+
+print(type(point))
+print(type(person))
+print(type(colour)) 
+
+fruits = {"Anitha","Sunitha","vinitha"}
+print(type(fruits))
+
+here ={1,2,3,4,5}
+print(type(here))
+
+uniq_number = {1,2,3,4,5}
+print(type(uniq_number))
